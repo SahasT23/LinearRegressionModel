97304b72299f8266ad129f3f1994efc8bf75057d
SahasT23 <sahas.st01@gmail.com>
2024-01-29 20:05:29 +0000

    added imports

--- .history/LRMMain_20240127161608.py
+++ .history/LRMMain_20240127161608.py
@@ -11,10 +11,4 @@ from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from scipy import stats
 from scipy.stats import kurtosis, skew
 
-path = r'/home/sahast/CodingProjects/oil_exxon.xlsx'
-price_data = pd.read_excel(path)
-
-price_data.index = pd.to_datetime(price_data['date'])
-price_data = price_data.drop(['date'], axis = 1)
-
-price_data.head()
+path =r/
